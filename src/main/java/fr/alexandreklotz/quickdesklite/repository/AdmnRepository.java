0f4f80236175b785e4f0469f60804cf39a2b1640
alexandreklotz <85607721+alexandreklotz@users.noreply.github.com>
2022-01-13 14:52:36 +0100

    roll back

--- src/main/java/fr/alexandreklotz/quickdesklite/repository/AdmnRepository.java
+++
@@ -1,9 +0,0 @@
-package fr.alexandreklotz.quickdesklite.repository;
-
-import fr.alexandreklotz.quickdesklite.model.Admn;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AdmnRepository extends JpaRepository<Admn, Long> {
-}
--- src/main/java/fr/alexandreklotz/quickdesklite/repository/TicketRepository.java
+++
@@ -1,9 +0,0 @@
-package fr.alexandreklotz.quickdesklite.repository;
-
-import fr.alexandreklotz.quickdesklite.model.Ticket;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TicketRepository extends JpaRepository<Ticket, Long> {
-}
